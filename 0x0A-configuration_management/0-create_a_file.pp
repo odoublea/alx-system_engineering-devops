30406763ba73c6f14e653bda934f8f1a3155243c
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-22 00:10:34 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#Create a file using manifest
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-  }
-
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-#This manifest execute a commannd
-exec { 'pkill':
-  command  => '/usr/bin/pkill killmenow',
-  provider => shell,
-  returns  => [0, 1],
-}
