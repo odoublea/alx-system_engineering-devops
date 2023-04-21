a6c74c39b9f350fc00afcad8992dc40738a1032a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-22 00:50:47 +0100

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,7 @@
-
-file { 'file':
-  path => /tmp/school
-  permission => 0744
-  owner => www-data
-  group => www-data
-  contains => 'I love Puppet'
+file { 'main':
+  path => '/tmp/school'
+  mode => '0744'
+  owner => 'www-data'
+  group => 'www-data'
+  content => 'I love Puppet'
 }
-
-
