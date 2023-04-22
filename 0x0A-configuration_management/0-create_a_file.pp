ed1e1e08414f45869b5d09faa4055b46ccc5096b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-22 01:02:35 +0100

    file name is file path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,12 @@
+#Create a file using manifest
 node default {
 
-file { 'main':
-  path    => '/tmp/school'
-  mode    => '0744'
-  owner   => 'www-data'
-  group   => 'www-data'
-  content => 'I love Puppet'
+file { '/tmp/school':
+  # path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 
 }
