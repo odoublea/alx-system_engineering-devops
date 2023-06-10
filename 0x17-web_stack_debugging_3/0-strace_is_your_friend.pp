325e6b1f71cefb3f2fb9e41bb5844154633beda3
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-06-10 17:44:37 +0100

    web debugging 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix typo in WordPress settings file to allow Apache listen on port 500
+exec { 'Fix typo in filename':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+ i provider => shell,
+}
