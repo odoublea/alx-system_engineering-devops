a8063609290a3a069abbfda5da833cbd709f0174
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-08-10 21:10:56 +0100

    reformat

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,20 +9,25 @@ from sys import argv
 
 def number_of_subscribers(subreddit):
     '''
-    Get the number of subscribers
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+        Returns 0 if the subreddit is invalid.
     '''
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "'my-app/0.0.1'"}
+    headers = {"User-Agent": "unxoda"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         subscribers = data["data"]["subscribers"]
         return subscribers
-    elif response.status_code == 404:
-        return 0  # Subreddit not found
     else:
-        # Handle other error cases here
-        return 0
+        return 0  # Subreddit not found
 
 
 if __name__ == '__main__':
