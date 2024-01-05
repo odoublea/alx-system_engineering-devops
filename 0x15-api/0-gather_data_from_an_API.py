f7105bf966f9e4550d6f250bb672d079e6d26490
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-05 11:29:45 +0100

    Get done tasks 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,19 +5,24 @@ returns information about his/her TODO list progress.
 """
 
 import requests
-from sys import argv
+from sys import argv, exit
 
-user = argv[1]
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        exit(1)
 
-response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user}')
-name = response.json().get('name')
-response2 = requests.get(f'https://jsonplaceholder.typicode.com/users/{user}/todos')
-completed = 0
-task_title = []
-for _ in response2.json():
-    if _.get('completed') is True:
-        task_title.append(_.get('title'))
-        completed += 1
-print(f'Employee {name} is done with tasks({completed}/{len(response2.json())}):')
-for _ in task_title:
-    print('\t {}'.format(_))
+    user = argv[1]
+
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user}')
+    name = response.json().get('name')
+    response2 = requests.get(f'https://jsonplaceholder.typicode.com/users/{user}/todos')
+    completed = 0
+    task_title = []
+    for _ in response2.json():
+        if _.get('completed') is True:
+            task_title.append(_.get('title'))
+            completed += 1
+    print(f'Employee {name} is done with tasks({completed}/{len(response2.json())}):')
+    for _ in task_title:
+        print('\t {}'.format(_))
