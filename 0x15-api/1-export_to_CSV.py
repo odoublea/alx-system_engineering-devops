4f0523dcb01b09c87ef079bdca701981bd3469b1
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-05 12:44:50 +0100

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+
+First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/
+TOTAL_NUMBER_OF_TASKS):
+EMPLOYEE_NAME: name of the employee
+NUMBER_OF_DONE_TASKS: number of completed tasks
+TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed
+and non-completed tasks
+Second and N next lines display the title of completed tasks: TASK_TITLE
+(with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+
+import csv
+import requests
+from sys import argv, exit
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        exit(1)
+
+    if not argv[1].isdigit():
+        print("Employee id must be an integer")
+        exit(1)
+
+    employee_id = argv[1]
+
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(url)
+    employee_name = response.json().get('name')
+
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response2 = requests.get(url)
+    tasks = response2.json()
+
+    total_tasks = len(tasks)
+    completed_tasks = [task for task in tasks if task.get('completed')]
+    completed = len(completed_tasks)
+
+    print(f'Employee {employee_name} is done with tasks('
+          f'{completed}/{total_tasks}):')
+    for task in completed_tasks:
+        print(f'\t {task.get("title")}')
+        
+    with open('{}.csv'.format(employee_id), 'w') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            taskwriter.writerow([employee_id, employee_name,
+                                 task.get('completed'), task.get('title')])
